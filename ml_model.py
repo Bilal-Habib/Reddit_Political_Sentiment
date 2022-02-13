773c6f61ba7e9225747602515b934a25e95c3ad8
55638034 <bilal.habib67@gmail.com>
2022-02-13 09:43:39 +0000

    Preprocessing complete, ml model nearly complete, collected data from multiple subreddits

--- ml_model.py
+++ ml_model.py
@@ -1,6 +1,18 @@
+import keras.losses
+from tensorflow import keras
 import reddit_connection as rc
 import re as regex
 import toolz
+import numpy as np
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, GlobalAveragePooling1D, LSTM, Dropout
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from collections import Counter
+from random import shuffle
+# from sklearn.metrics import confusion_matrix
+import pickle
 
 
 def generateTrainingData(no_left_posts, no_right_posts):
@@ -13,9 +25,9 @@ def generateTrainingData(no_left_posts, no_right_posts):
     for comment in comments:
         left_wing_comments.append(comment.body)
     # usa left-wing party
-    # comments = rc.getSubredditComments('democrats', 100, 'top')
+    # comments = rc.getSubredditComments('democrats', no_left_posts, 'top')
     # for comment in comments:
-    #     left_wing_comments.append(comment)
+    #     left_wing_comments.append(comment.body)
 
     # gather right-wing data
     # uk right-wing party
@@ -23,35 +35,16 @@ def generateTrainingData(no_left_posts, no_right_posts):
     for comment in comments:
         right_wing_comments.append(comment.body)
     # usa right-wing party
-    # comments = rc.getSubredditComments('Republican', 100, 'top')
+    # comments = rc.getSubredditComments('Republican', no_right_posts, 'top')
     # for comment in comments:
-    #     right_wing_comments.append(comment)
+    #     right_wing_comments.append(comment.body)
 
     return [left_wing_comments, right_wing_comments]
 
 
 # preprocess data
 # stop words from NLTK
-stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves',
-              'you', "you're", "you've", "you'll", "you'd", 'your', 'yours',
-              'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's",
-              'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them',
-              'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this',
-              'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be',
-              'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing',
-              'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while',
-              'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through',
-              'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in',
-              'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
-              'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few',
-              'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own',
-              'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don',
-              "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y',
-              'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't",
-              'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma',
-              'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't",
-              'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't",
-              'wouldn', "wouldn't"]
+stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
 # words that change meaning of sentiment: "you've", "you'll", "you'd", "that'll", "should've",
 # couldn', "couldn't", didn', "didn't", 'doesn', "doesn't",
@@ -73,26 +66,49 @@ stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves',
 
 
 # function from website
+# def filterComment(comment):
+#     # Take care of non-ascii characters
+#     comment = (comment.encode('ascii', 'replace')).decode("utf-8")
+#
+#     # Convert text to Lower case
+#     comment = comment.lower()
+#
+#     # filter comment by removing links
+#     # url = regex.compile(r"https?//\S+|www\.\S+")
+#     # removed_url = url.sub(r"", comment)
+#     removed_url = regex.sub(r"https?//\S+|www\.\S+", '', comment)
+#
+#     # Remove multiple spacing
+#     space = regex.sub(r"\s+", " ", removed_url, flags=regex.I)
+#
+#     # Remove special characters
+#     clean_text = regex.sub(r'[!"#$%&()*+,-./:;?@[\]^_`{|}~]', ' ', space)
+#     space1 = regex.sub(r"\s+", " ", clean_text, flags=regex.I)
+#
+#     comment_without_noise = [word for word in space1.split() if word not in stop_words]
+#     filtered_comment = ' '.join(comment_without_noise)
+#     return filtered_comment
+
+
 def filterComment(comment):
-    # Take care of non-ascii characters
-    comment = (comment.encode('ascii', 'replace')).decode("utf-8")
+    return regex.sub(r"(https?://|www)\S+|\w*\d\w*|[^A-Za-z\s.,'’?]+", "", comment)
 
-    # Convert text to Lower case
-    comment = comment.lower()
-
-    # filter comment by removing links
-    removed_link = regex.sub(r'http\S+', '', comment)
-
-    # Remove multiple spacing
-    space = regex.sub(r"\s+", " ", removed_link, flags=regex.I)
-
-    # Remove special characters
-    clean_text = regex.sub(r'[!"#$%&()*+,-./:;?@[\]^_`{|}~]', ' ', space)
-    space1 = regex.sub(r"\s+", " ", clean_text, flags=regex.I)
-
-    comment_without_noise = [word for word in space1.split() if word not in stop_words]
-    filtered_comment = ' '.join(comment_without_noise)
-    return filtered_comment
+#
+# def filterComment(data):
+#     # Removing URLs with a regular expression
+#     url_pattern = regex.compile(r'https?://\S+|www\.\S+')
+#     data = url_pattern.sub(r'', data)
+#
+#     # Remove Emails
+#     data = regex.sub('\S*@\S*\s?', '', data)
+#
+#     # Remove new line characters
+#     data = regex.sub('\s+', ' ', data)
+#
+#     # Remove distracting single quotes
+#     data = regex.sub("\'", "", data)
+#
+#     return data
 
 
 # function that applies filter to every comment
@@ -124,15 +140,19 @@ def labelComments(left_wing_comments, right_wing_comments):
     return [labelled_left, labelled_right]
 
 
-# function that combines the left and right comments
+# function that retrieves the comments and labels separately
 def getCommentsLabels(labelled_left, labelled_right):
-    # combined_comments = labelled_left + labelled_right
     comments = []
     labels = []
 
     # use interleave function to combine comments & labels in alternating fashion
-    combined_comments = toolz.interleave([labelled_left, labelled_right])
+    # avoids bias in learning stage
+    # combined_comments = toolz.interleave([labelled_left, labelled_right])
 
+    combined_comments = labelled_left + labelled_right
+    shuffle(combined_comments)
+
+    # separate comments and labels
     for comment in combined_comments:
         comments.append(comment[0])
         labels.append(comment[1])
@@ -140,60 +160,160 @@ def getCommentsLabels(labelled_left, labelled_right):
     return [comments, labels]
 
 
-# create ml model
+# counts number of times words appear
+def wordCounter(comments):
+    count = Counter()
+    for comment in comments:
+        for word in comment.split():
+            count[word] += 1
+    return count
+
+
+# format data appropriately and generate ml model
 def createModel(left_wing, right_wing):
     # filter comments
-    filtered_left = filterAllComments(left_wing)
-    filtered_right = filterAllComments(right_wing)
+    filtered_left, filtered_right = filterAllComments(left_wing), filterAllComments(right_wing)
 
     # label comments
     labelled_comments = labelComments(filtered_left, filtered_right)
-    labelled_left = labelled_comments[0]
-    labelled_right = labelled_comments[1]
+    labelled_left, labelled_right = labelled_comments[0], labelled_comments[1]
 
     # set comments and labels
     comments_labels = getCommentsLabels(labelled_left, labelled_right)
-    X = comments_labels[0]
-    y = comments_labels[1]
+    comments, labels = comments_labels[0], comments_labels[1]
 
     # split both into training/testing (start with 70/30)
-    n_samples = len(comments)
-    percentage_samples = int(0.7 * n_samples)
-    X_train, X_test = X[:percentage_samples], X[percentage_samples:]
-    y_train, y_test = y[:percentage_samples], y[percentage_samples:]
-
-    # create ml model
-
+    n_comments = len(comments)
+    train_size = int(0.80 * n_comments)
+    training_comments, testing_comments = comments[:train_size], comments[train_size:]
+    training_labels, testing_labels = labels[:train_size], labels[train_size:]
+    print('INITIAL TRAIN SIZE: ', train_size)
 
 
-# get accuracy by testing it on testing data
+    # ----------------------------------------------
+    # he said forget about testing part, split training data into 80:20 again
+    # n_comments = len(training_comments)
+    # train_size = int(0.80 * n_comments)
+    # print('SECOND TRAIN SIZE: ', train_size)
+    # training_comments2, val_comments = training_comments[:train_size], training_comments[train_size:]
+    # training_labels2, val_labels = training_labels[:train_size], training_labels[train_size:]
+    # use val labels
+    # ----------------------------------------------
 
 
-# X = comments text "I like boris"
-# y = comments label "0"
+    # dictionary of words and their counter
+    counter = wordCounter(comments)
+    # counter = wordCounter(training_comments)
+
+    # number of unique words
+    num_unique_words = len(counter)
+
+    # vectorise text corpus by turning each comment into a sequence of integers
+    # tokenizer = Tokenizer(num_words=num_unique_words)
+    tokenizer = Tokenizer(oov_token='<OOV>')
+    tokenizer.fit_on_texts(training_comments)
+    # tokenizer.fit_on_texts(training_comments2)
+    # word_index = tokenizer.word_index
+
+    # he said change testing comments to val comments
+    train_sequences = tokenizer.texts_to_sequences(training_comments)
+    val_sequences = tokenizer.texts_to_sequences(testing_comments)
+    # train_sequences = tokenizer.texts_to_sequences(training_comments2)
+    # val_sequences = tokenizer.texts_to_sequences(val_comments)
+
+    # we want every sequence to have same length so we'll use padding
+    max_length = 500
+    training_padded = pad_sequences(train_sequences, maxlen=max_length, padding="post", truncating="post")
+    val_padded = pad_sequences(val_sequences, maxlen=max_length, padding="post", truncating="post")
+
+    print("padded shape: ", training_padded.shape, val_padded.shape)
+
+    # model from youtube
+    model = Sequential()
+    model.add(Embedding(100000, 64, input_length=max_length))
+    model.add(GlobalAveragePooling1D())
+    model.add(Dense(24, activation="relu"))
+    # model.add(Dense(32, activation="tanh"))
+    # model.add(LSTM(64, activation='relu'))  # Our LSTM layer
+    # model.add(Dense(24, activation="relu", kernel_regularizer=keras.regularizers.L1(0.01)))
+    # model.add(Dropout(0.20))
+    model.add(Dense(1, activation='sigmoid'))
+
+    # loss = keras.losses.BinaryCrossentropy(from_logits=False)
+    # opt = keras.optimizers.Adam(learning_rate=0.01)
+    # metrics = ["accuracy"]
+    # model.compile(loss=loss, optimizer=opt, metrics=metrics)
+
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.summary()
+    model.fit(np.asarray(training_padded), np.asarray(training_labels), epochs=20,
+              validation_data=(np.asarray(val_padded), np.asarray(testing_labels)), verbose=2, shuffle=True)
+
+    # predictions = model.predict(training_padded)
+    # predictions = [1 if p > 0.5 else 0 for p in predictions]
+    #
+    # print(training_comments[10:20])
+    # print(training_labels[10:20])
+    # print(predictions[10:20])
+
 
 if __name__ == '__main__':
-    comments = generateTrainingData(1, 1)
+    # pass
+    # comments = generateTrainingData(100, 100)
+    # comments = generateTrainingData(100, 100)
     # raw comments
-    left_wing = comments[0]
-    right_wing = comments[1]
+    # left_wing = pickle.load(open("three_left.txt"))
+    # right_wing = pickle.load(open("three_right.txt"))
+    # # left_wing, right_wing = comments[0], comments[1]
+    # print('no of left wing comments: ', len(left_wing))
+    # print('no of right wing comments: ', len(right_wing))
+    # createModel(left_wing, right_wing)
+    # leftfile = open("left_wing.txt", "w")
+    # for element in left_wing:
+    #     leftfile.write(element + "\n")
+    # leftfile.close()
+    #
+    # rightfile = open("right_wing.txt", "w")
+    # for element in right_wing:
+    #     rightfile.write(element + "\n")
+    # rightfile.close()
+
+
+    # code to read from file
+    left_wing = []
+    right_wing = []
+
+    with open('three_left.txt') as f:
+        left_wing = [line.rstrip('\n') for line in f]
+
+    with open('three_right.txt') as f:
+        right_wing = [line.rstrip('\n') for line in f]
+
+    print('no of left wing comments: ', len(left_wing))
+    print('no of right wing comments: ', len(right_wing))
+    createModel(left_wing, right_wing)
     # print("raw: ", left_wing, right_wing)
-
-    # filtered comments
-    filtered_left = filterAllComments(left_wing)
-    filtered_right = filterAllComments(right_wing)
-    # print("filtered: ", filtered_left, filtered_right)
-
-    # labelled comments
-    labelled_comments = labelComments(filtered_left, filtered_right)
-    labelled_left = labelled_comments[0]
-    labelled_right = labelled_comments[1]
-    # print("labelled: ", labelled_left, labelled_right)
-
-    # combine comments
-    comments_labels = getCommentsLabels(labelled_left, labelled_right)
-    comments = comments_labels[0]
-    labels = comments_labels[1]
-    print("comments: ", comments)
-    # print("labels: ", labels)
+    #
+    # # filtered comments
+    # filtered_left = filterAllComments(left_wing)
+    # filtered_right = filterAllComments(right_wing)
+    # # print("filtered: ", filtered_left, filtered_right)
+    #
+    # # labelled comments
+    # labelled_comments = labelComments(filtered_left, filtered_right)
+    # labelled_left = labelled_comments[0]
+    # labelled_right = labelled_comments[1]
+    # # print("labelled: ", labelled_left, labelled_right)
+    #
+    # # combine comments
+    # comments_labels = getCommentsLabels(labelled_left, labelled_right)
+    # comments = comments_labels[0]
+    # labels = comments_labels[1]
+    # # print("comments: ", comments)
+    # # print("labels: ", labels)
     # print(labels.count(0), labels.count(1))
+    # # counter = wordCounter(comments)
+    # # print(len(counter))
+    # # print(counter)
+    # # print(counter.most_common(5))
+
--- reddit_connection.py
+++ reddit_connection.py
@@ -65,18 +65,20 @@ def getSubredditComments(page_name, no_posts, post_sort_type):
         post.comments.replace_more(limit=0)
         post.comment_sort = comment_sort_type
         scraped_comments += post.comments
-    return scraped_comments
+    replies = getSubredditReplies(scraped_comments)
+    all_comments = scraped_comments + replies
+    return all_comments
 
 
-# def getSubredditReplies(comments):
-#     reply_sort_type = "top"
-#     scraped_replies = []
-#     for comment in comments:
-#         comment.reply_sort = reply_sort_type
-#         if len(comment.replies) > 0:
-#             for reply in comment.replies:
-#                 scraped_replies.append(reply)
-#     return scraped_replies
+def getSubredditReplies(comments):
+    reply_sort_type = "top"
+    scraped_replies = []
+    for comment in comments:
+        comment.reply_sort = reply_sort_type
+        if len(comment.replies) > 0:
+            for reply in comment.replies:
+                scraped_replies.append(reply)
+    return scraped_replies
 
 
 def getUserComments(reddit_username, no_comments, comment_sort_type):
@@ -91,3 +93,99 @@ def getUserComments(reddit_username, no_comments, comment_sort_type):
         for comment in reddit.redditor(reddit_username).comments.top(limit=no_comments):
             scraped_comments.append(comment)
     return scraped_comments
+
+
+def flatten_list(elem):
+    """ a better flat_list() function than pythons default implementation for this specific use case """
+    if not elem:
+        return []
+    elif isinstance(elem, tuple):
+        return [elem]
+    elif isinstance(elem, list):
+        combined = []
+        for element in elem:
+            flat = flatten_list(element)
+            if flat:
+                combined = combined + flat
+        return combined
+    else:
+        print(type(elem))
+        print(elem)
+
+
+def get_comments(comments):
+    """ Given a comment object (i.e CommentForest), outputs a list of all the comments in there excluding the
+    MoreComment objects and replies """
+    if comments is None:
+        return []
+    elif isinstance(comments, praw.models.reddit.more.MoreComments):
+        return []
+    elif isinstance(comments, praw.models.reddit.comment.Comment):
+        author = None
+        return [(comments.body, author)]
+    elif isinstance(comments, praw.models.comment_forest.CommentForest):
+        combined = []
+        for comment in (comments.list()):
+            combined = combined + get_comments(comment)
+        return combined
+    elif isinstance(comments, list):
+        return []
+    else:
+        print(type(comments))
+        print(comments)
+
+
+def get_submission_from_subreddit(reddit, subreddit_name, num_posts=1, sort_order="top"):
+    """ Returns a list of submission objects found on the specified subreddit """
+    submissions = reddit.subreddit(subreddit_name)
+    if sort_order == "top":
+        return submissions.top(limit=num_posts)
+    elif sort_order == "hot":
+        return submissions.hot(limit=num_posts)
+    else:
+        return submissions.new(limit=num_posts)
+
+
+def get_all_comments_from_subreddit(subreddit_name, num_posts, sort_order="top"):
+    """ Given a subreddit name, will fetch all comments from there """
+    # get a new instance of an api connection to reddit using the PRAW api
+    comments_acc = set()
+    for submission in get_submission_from_subreddit(reddit, subreddit_name, num_posts, sort_order):
+        # get comments and also get nested replies if that option was selected on the frontend
+        comments_acc.update(flatten_list(get_comments(submission.comments)))
+    return list(comments_acc)
+
+
+if __name__ == '__main__':
+    import pickle
+
+    uk_left = get_all_comments_from_subreddit('labouruk', 200)
+    usa_left = get_all_comments_from_subreddit('democrats', 200)
+    canada_left = get_all_comments_from_subreddit('ndp', 200)
+    all_left = uk_left + usa_left + canada_left
+
+    uk_right = get_all_comments_from_subreddit('tories', 200)
+    usa_right = get_all_comments_from_subreddit('Republican', 200)
+    canada_right = get_all_comments_from_subreddit('CanadianConservative', 200)
+    all_right = uk_right + usa_right + canada_right
+
+    print(len(all_left), len(all_right))
+
+    leftfile = open("three_left.txt", "wb")
+    pickle.dump(all_left, leftfile)
+    leftfile.close()
+
+    rightfile = open("three_right.txt", "wb")
+    pickle.dump(all_right, rightfile)
+    rightfile.close()
+
+    # left_wing = []
+    # right_wing = []
+    #
+    # with open('three_left.txt') as f:
+    #     left_wing = [line.rstrip('\n') for line in f]
+    #
+    # with open('three_right.txt') as f:
+    #     right_wing = [line.rstrip('\n') for line in f]
+    #
+    # print(len(left_wing), len(right_wing))
